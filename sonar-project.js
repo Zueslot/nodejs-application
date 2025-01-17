bcf570dfbde1cfe9176b5a91b068bd9e5de17a01
Zueslot <koolharmbhrosz1@gmail.com>
2025-01-16 19:39:39 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': 'sqa_fc09e9cce86ed391b57dafffa25bab5d98e63122',
+	    'sonar.login': 'sqa_0def991eb51ab6ff80a4128d907de90794aed26f',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
